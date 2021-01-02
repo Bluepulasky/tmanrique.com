105613e8f448d889dfdee676299d109016b9ed2a
Manwe <manuel.etchegaray7@gmail.com>
2021-01-02 18:41:59 -0300

    seleccionar ruta actual

--- src/top-nav/index.jsx
+++ src/top-nav/index.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./index.scss";
 
 export default function TopNav() {
+  const location = useLocation();
+
   return (
     <div className="top-nav">
       <div className="container_left">
-        <Link to="/">Tomas Manrique</Link>
+        <Link  to="/">
+          Tomas Manrique
+        </Link>
       </div>
       <div className="container_right">
-        <Link to="/">Work</Link>
-        <Link to="/contact">Contact</Link>
+        <Link className={location.pathname === "/" ? "selected" : ""} to="/">Work</Link>
+        <Link className={location.pathname === "/contact" ? "selected" : ""} to="/contact">Contact</Link>
       </div>
     </div>
   );
