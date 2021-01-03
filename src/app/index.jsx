e513fdfb2a5f73988547e7cb0b58c9ef4673c63e
Manwe <manuel.etchegaray7@gmail.com>
2021-01-03 19:12:10 -0300

    titulo

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./index.scss";
-import { Route } from "react-router-dom";
+import { Route, useLocation } from "react-router-dom";
 import Home from "../home";
 import RenoRun from "../renorun";
 import TopNav from "../top-nav";
 import Footer from "../footer";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    switch (location.pathname) {
+      case "/renorun":
+        document.title = "Tomas Manrique - RenoRun";
+        break;
+      default:
+        document.title = "Tomas Manrique - Work";
+        break;
+    }
+  }, [location]);
+
   return (
     <div className="wrapper">
       <TopNav />
