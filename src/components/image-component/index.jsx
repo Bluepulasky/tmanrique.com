e9422741b88d81d0992fddf21342d3ab1d1fc5d5
Manwe <manuel.etchegaray7@gmail.com>
2021-01-04 11:00:45 -0300

    useScrollOnHash

--- src/components/image-component/index.jsx
+++ src/components/image-component/index.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
 import "./index.scss";
+import useScrollOnHash from "../../hooks/useScrollOnHash";
 
 export default function ImageComponent(props) {
   const { image, title, linktarget } = props;
 
   const history = useHistory();
 
+  const divRef = useRef(null);
+  useScrollOnHash(divRef, title);
+
   return (
-    <div className="image-container">
+    <div ref={divRef} className="image-container">
       <div
         className="image"
         onClick={() => history.push(linktarget)}
